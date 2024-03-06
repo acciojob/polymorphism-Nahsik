d61b1e7c9798f3596cac80741581100986d9be16
kishan <kishan.dubariya@phenompeople.com>
2024-03-07 00:38:56 +0530

    1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,10 @@ package com.driver;
 public class Main {
 
     public static void main(String[] arg) {
-        Product product = new Product();
-        System.out.println(product.product(2, 3));
-        System.out.println(product.product(2, 3, 4));
-        System.out.println(product.product((double) 2, (double) 3));
+        Product p = new Product();
+        System.out.println(p.product(2, 3));
+        System.out.println(p.product(2, 3, 4));
+        System.out.println(p.product((double) 2, (double) 3));
     }
 }
 
