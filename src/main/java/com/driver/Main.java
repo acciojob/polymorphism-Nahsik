2b6c7ddaf8cc142a5e4c8551cab22d15f42d2b1e
kishan <kishan.dubariya@phenompeople.com>
2024-03-07 23:07:52 +0530

    test

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,10 +2,18 @@ package com.driver;
 
 public class Main {
 
-    public static void main(String[] arg) {
-        Product p = new Product();
-        System.out.println(p.product(2, 3));
-        System.out.println(p.product(2, 3, 4));
-        System.out.println(p.product((double) 2, (double) 3));
+    public static class Product {
+
+        public int product(int x, int y) {
+            return (x * y);
+        }
+
+        public int product(int x, int y, int z) {
+            return (x * y * z);
+        }
+
+        public double product(double x, double y) {
+            return (x * y);
+        }
     }
 }
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,17 +2,11 @@ package com.driver.test;
 
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
-import com.driver.Product;
 
 import junit.framework.Assert;
 
 public class TestCases {
 
-    private Product p = new Product();
 
-    @Test
-    public void validateTest(){
-      Assert.assertEquals(6, p.product(2, 3));
-    }
      
 }
