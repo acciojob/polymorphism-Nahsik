3a1612987716c77e8c2415b991f512537e66af94
kishan <kishan.dubariya@phenompeople.com>
2024-03-07 22:46:25 +0530

    new commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,18 +9,3 @@ public class Main {
         System.out.println(p.product((double) 2, (double) 3));
     }
 }
-
-class Product {
-    public int product(int x, int y) {
-        return x * y;
-    }
-
-    public int product(int x, int y, int z) {
-        return x * y * z;
-    }
-
-    public double product(double x, double y) {
-        return x * y;
-    }
-
-}
