aaa6ad0aed9d2546411a3c4bc092d17611ecd0e6
kishan <kishan.dubariya@phenompeople.com>
2024-03-07 23:02:31 +0530

    added test cases

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,7 +2,17 @@ package com.driver.test;
 
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
+import com.driver.Product;
+
+import junit.framework.Assert;
 
 public class TestCases {
+
+    private Product p = new Product();
+
+    @Test
+    public void validateTest(){
+      Assert.assertEquals(6, p.product(2, 3));
+    }
      
 }
